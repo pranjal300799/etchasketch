52254f8aeda3e277fbdfe2bc92c7c0d92150cceb
pranjal300799 <pranjalkumarkunik@gmail.com>
2023-01-11 20:22:48 +0530

    Add createDivs() to ad the grid
    Addedthe createDivs() function that adds the smaller squares
    inside the sketchpad based on the input from the prompt

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+   let sketchpad=document.querySelector(".sketchpad");
+   let numberOfSquares=+prompt('no of squares on each edge');
+   addDivs(numberOfSquares);
+function addDivs(number)
+{
+   for(let i=0;i<number**2;i++)
+   {   let square=document.createElement('div');
+       square.style["width"]=`${(800/number-2)}px`;
+       square.style["height"]=`${(800/number-2)}px`;
+       sketchpad.appendChild(square);
+   }
+}
