9d1595ac12b704723d03ec4aced8223395df947f
pranjal300799 <pranjalkumarkunik@gmail.com>
2023-01-22 16:36:38 +0530

    Add reference of sketchpad inside the addDivs()
    added the reference to sketchpad inside the addDivs()
    which malfunctioned due to removal of initial prompts for
    no. of squares input.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@ let state='off';
 function addDivs(number)
 {
    for(let i=0;i<number**2;i++)
-   {   let square=document.createElement('div');
+   {   const sketchpad=document.querySelector('.sketchpad');
+       let square=document.createElement('div');
        square.style["width"]=`${(800/number)}px`;
        square.style["height"]=`${(800/number)}px`;
        sketchpad.appendChild(square);
